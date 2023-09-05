661f4b5ffba681caebcd543391a2829e21ad39a8
GetCodeWidUmair <144023398+Getcodewidumair@users.noreply.github.com>
2023-09-05 19:47:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function scrolltop() {
+    let a = window.document;
+    a = location.href = '#home';
+}
+// let contact = document.getElementById('contactForm');
+// contact.addEventListener('submit', function (e) {
+//     e.preventDefault();
+//     alert(`Sorry Request cannot be accepted due to some technical errors`);
+// })function scrolltop() {
+//     let a = window.document;
+//     a = location.href = '#home';
+// }
+// let contact = document.getElementById('contactForm');
+// contact.addEventListener('submit', function (e) {
+//     e.preventDefault();
+//     alert(`Sorry Request cannot be accepted due to some technical errors`);
+// })
